435ccf61fbfeebaa11cf9c9bfb590a053692916a
Guilherme Martins <guimartins_9@hotmail.com>
2021-09-22 18:28:04 -0300

    Termina o requisito 12

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const eMail = document.getElementById('input-email');
+const eMail = document.getElementById('input-email-login');
 const password = document.getElementById('input-password');
 const login = document.getElementById('button-login');
 
-login.addEventListener("click", function (){
-    if ( eMail.value !== 'tryber@teste.com' && password.value !== '1234546') {
-      alert('Email ou senha inválidos.');
+login.addEventListener('click', () => {
+  if (eMail.value !== 'tryber@teste.com' && password.value !== '1234546') {
+    alert('Email ou senha inválidos.');
   } else {
-      alert('Olá, Tryber!');
+    alert('Olá, Tryber!');
   }
 });
