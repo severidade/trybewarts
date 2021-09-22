932f9840bf37d77f5d118a4dd82053068a0dbdcc
severo <oliveira.severo@gmail.com>
2021-09-22 16:36:50 -0300

    Formulario de login refatorado

--- script.js
+++ script.js
@@ -2,22 +2,10 @@ const eMail = document.getElementById('input-email');
 const password = document.getElementById('input-password');
 const login = document.getElementById('button-login');
 
-function validateLogin() {
-  if ( eMail.value !== 'tryber@teste.com' && password.value !== '1234546') {
-    alert("Email ou senha inválidos.");
+login.addEventListener("click", function (){
+    if ( eMail.value !== 'tryber@teste.com' && password.value !== '1234546') {
+      alert('Email ou senha inválidos.');
   } else {
-    alert("Olá, Tryber!");
+      alert('Olá, Tryber!');
   }
-
-  // alert("passou");
-}
-
-login.addEventListener("click", validateLogin);
-
-// login.addEventListener("click", function (){
-//     if ( eMail.value !== 'tryber@teste.com' && password.value !== '1234546') {
-//     alert('Email ou senha inválidos.');
-//   } else {
-//     alert('Olá, Tryber!');
-//   }
-// });
+});
