f8118209b8052aebc9276401eafe584831cc8dce
severo <oliveira.severo@gmail.com>
2021-09-24 09:14:12 -0300

    Requisitos 17 e 18 finalizados

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const eMail = document.getElementById('input-email-login');
 const password = document.getElementById('input-password');
 const login = document.getElementById('button-login');
+const agreement = document.getElementById('agreement');
+const submit = document.getElementById('submit-btn');
 
 login.addEventListener('click', () => {
   if (eMail.value !== 'tryber@teste.com' && password.value !== '1234546') {
@@ -9,3 +11,11 @@ login.addEventListener('click', () => {
     alert('Olá, Tryber!');
   }
 });
+
+agreement.addEventListener('click', () =>{
+  if(agreement.checked){
+    submit.disabled = false;
+  } else {
+    submit.disabled = true;
+  }
+}
